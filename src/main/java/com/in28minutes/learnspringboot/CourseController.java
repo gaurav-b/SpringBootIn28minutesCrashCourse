1be76941eaec3263fa0a9d681efbe0b69e555fb4
gaurav-b <sunymca619@gmail.com>
2022-11-07 23:54:22 +0530

    C:/Program Files/Git/courses exposed

---
+++ src/main/java/com/in28minutes/learnspringboot/CourseController.java
@@ -0,0 +1,38 @@
+package com.in28minutes.learnspringboot;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gaura
+ *
+ * with this API we need a URL setup as /courses
+ * 
+ * data returned would be,
+ * 
+ * [
+ * 	{
+ * 		"id": 1,
+ * 		"name": "Learn AWS"
+ * 		"author": "in28minutes"
+ * 	}
+ * ]
+ */
+
+
+@RestController
+public class CourseController {
+
+	@GetMapping("/courses")
+	public List<Course> retrieveAllCourse() {
+		
+		return Arrays.asList(
+				new Course(1, "Learn AWS", "in28minutes"),
+				new Course(1, "Learn DevOps", "in28minutes")
+		);
+	}
+	
+}
